12691d36cc679849c7ad9e1e9de28e6919bd909c
Chris Chung <christopher0chung@gmail.com>
2017-02-13 17:50:59 -0500

    SteamVR and Bloom effect.  Native controller tracking started.

---
+++ VR Studio - Week 3/Assets/NativeVRController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.VR;
+
+public class NativeVRController : MonoBehaviour {
+
+    public Transform leftHand, rightHand;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        leftHand.localPosition = InputTracking.GetLocalPosition(VRNode.LeftHand);
+        rightHand.localPosition = InputTracking.GetLocalPosition(VRNode.RightHand);
+
+        leftHand.localRotation = InputTracking.GetLocalRotation(VRNode.LeftHand);
+        rightHand.localRotation = InputTracking.GetLocalRotation(VRNode.RightHand);
+    }
+}
